18d14e18d8a33247f8aab3548ed3966501d65a6c
Victor Cazanave <cazanave.v@sfeir.com>
2014-08-15 14:25:12 +0200

    [multiselect] Add event on add/remove word

--- multiselect/dev/app.js
+++ multiselect/dev/app.js
@@ -1,5 +1,12 @@
 var app = angular.module('app', []);
 
-app.controller('MainCtrl', ['$scope', function($scope) {
+app.controller('MainCtrl', ['$scope',
+  function($scope) {
 
-}]);
+    $scope.words = '';
+
+    $scope.$on('MS_EVENT-change', function(evt, values) {
+      $scope.words = values.join(',');
+    });
+  }
+]);
--- multiselect/dev/widget/multiselect.js
+++ multiselect/dev/widget/multiselect.js
@@ -5,17 +5,18 @@ app.directive('ngMultiselect', function() {
     replace: true,
     scope: {},
     link: function(scope, element, attrs) {
-      scope.word = "";
+      scope.word = '';
       scope.words = [];
       scope.showError = false;
-      scope.error = "Already added !";
+      scope.error = 'Already added !';
 
       scope.addWord = function() {
         scope.showError = false;
-        if (scope.word !== "") {
+        if (scope.word !== '') {
           if (scope.words.indexOf(scope.word) === -1) {
             scope.words.push(scope.word);
-            scope.word = "";
+            scope.$emit('MS_EVENT-change', scope.words);
+            scope.word = '';
           } else {
             scope.showError = true;
           }
@@ -24,15 +25,17 @@ app.directive('ngMultiselect', function() {
 
       scope.removeWord = function(index) {
         scope.words.splice(index, 1);
+        scope.$emit('MS_EVENT-change', scope.words);
       };
 
       scope.removeLastWord = function() {
         scope.words.pop();
+        scope.$emit('MS_EVENT-change', scope.words);
       };
 
       scope.keydown = function(event) {
         scope.showError = false;
-        if (scope.word === "" && scope.words.length > 0 && event.keyCode === 8) {
+        if (scope.word === '' && scope.words.length > 0 && event.keyCode === 8) {
           scope.removeLastWord();
         }
       };
