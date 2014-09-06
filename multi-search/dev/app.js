3c4bbf4bf99b7cda9025138a8a6949e17adc88b8
Victor Cazanave <cazanave.v@sfeir.com>
2014-09-06 16:49:17 +0200

    [multi-search] Add model attribute

--- multi-search/dev/app.js
+++ multi-search/dev/app.js
@@ -3,10 +3,10 @@ var app = angular.module('app', []);
 app.controller('MainCtrl', ['$scope',
   function($scope) {
 
-    $scope.words = '';
+    // $scope.words = '';
 
-    $scope.$on('MS_EVENT-change', function(evt, values) {
-      $scope.words = values.join(', ');
-    });
+    // $scope.$on('MS_EVENT-change', function(evt, values) {
+    //   $scope.words = values.join(', ');
+    // });
   }
 ]);
--- multi-search/dev/widget/multi-search.js
+++ multi-search/dev/widget/multi-search.js
@@ -1,21 +1,27 @@
 app.directive('ngMultiSearch', function() {
   return {
     restrict: 'AE',
-    templateUrl: 'widget/multi-search.html',
+    templateUrl: 'widget/multi-search.tpl.html',
     replace: true,
-    scope: {},
+    scope: {
+      words: '=model'
+    },
     link: function(scope, element, attrs) {
+
       scope.word = '';
-      scope.words = [];
+      scope.words = scope.words || [];
       scope.showError = false;
       scope.error = 'Already added !';
 
+      /**
+       * Add word in words list
+       */
       scope.addWord = function() {
         scope.showError = false;
         if (scope.word !== '') {
           if (scope.words.indexOf(scope.word) === -1) {
             scope.words.push(scope.word);
-            scope.$emit('MS_EVENT-change', scope.words);
+            //scope.$emit('MS_EVENT-change', scope.words);
             scope.word = '';
           } else {
             scope.showError = true;
@@ -23,25 +29,37 @@ app.directive('ngMultiSearch', function() {
         }
       };
 
+      /**
+       * Remove word from words list
+       */
       scope.removeWord = function(index) {
         scope.words.splice(index, 1);
-        scope.$emit('MS_EVENT-change', scope.words);
+        //scope.$emit('MS_EVENT-change', scope.words);
       };
 
-      scope.removeLastWord = function() {
-        scope.words.pop();
-        scope.$emit('MS_EVENT-change', scope.words);
+      /**
+       * Get width of words list to set dynamically width of HTML element
+       */
+      scope.getWordsWidth = function() {
+        return angular.element(document.querySelector('#MS_words-width'))[0].offsetWidth;
       };
 
+      /**
+       * Manage keydown events
+       */
       scope.keydown = function(event) {
         scope.showError = false;
         if (scope.word === '' && scope.words.length > 0 && event.keyCode === 8) {
-          scope.removeLastWord();
+          removeLastWord();
         }
       };
 
-      scope.getWordsWidth = function() {
-        return angular.element(document.querySelector('#MS_words-width'))[0].offsetWidth;
+      /**
+       * Remove the last word from words list
+       */
+      var removeLastWord = function() {
+        scope.words.pop();
+        //scope.$emit('MS_EVENT-change', scope.words);
       };
     }
   };
